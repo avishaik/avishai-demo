3ba827c27ffe6ba17425da31487fbb68e50d77d2
avishaik <avishai.klaiman@gmail.com>
2025-01-22 22:15:55 +0200

    add facebook page

--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -7,7 +7,18 @@ export const TasArea: FC = (): ReactElement => {
         <Grid2 size={{ xs: 6, md: 8 }} >
             <h2>Welcome to Avishai's website!</h2>
             <h2>We are still under construction, please be patient...</h2>
+
+            <h3>
+                For more details: 
+                <a 
+                    href={"https://facebook.com/avishai.klaiman"}>
+                    <img src={require("../../images/vintage-icon-png-19160.png")} alt="facebook" />
+                </a>
+            </h3>
             <img src={require("../../images/Under-Construction-Transparent-Images.png")} alt="under constrcution" />
+            
+
+            
       </Grid2>
     );
 }
